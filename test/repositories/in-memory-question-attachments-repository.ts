23ca38109acf557cde9efd4675bb9e3ebc00e68f
gbrogni <harden13brogni@gmail.com>
2024-02-16 20:21:01 -0300

    .

---
+++ test/repositories/in-memory-question-attachments-repository.ts
@@ -0,0 +1,24 @@
+import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
+import { QuestionAttachmentsRepository } from './question-attachments-repository'
+
+export class InMemoryQuestionAttachmentsRepository
+  implements QuestionAttachmentsRepository
+{
+  public items: QuestionAttachment[] = []
+
+  async findManyByQuestionId(questionId: string) {
+    const questionAttachments = this.items.filter(
+      (item) => item.questionId.toString() === questionId,
+    )
+
+    return questionAttachments
+  }
+
+  async deletManyByQuestionId(questionId: string) {
+    const questionAttachments = this.items.filter(
+      (item) => item.questionId.toString() !== questionId,
+    )
+
+    this.items = questionAttachments
+  }
+}
---
+++ test/repositories/in-memory-questions-repository.ts
@@ -0,0 +1,63 @@
+import { PaginationParams } from '@/core/repositories/pagination.param'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { Question } from '@/domain/forum/enterprise/entities/question'
+import { QuestionAttachmentsRepository } from './question-attachments-repository'
+import { DomainEvents } from '@/core/events/domain-events'
+
+export class InMemoryQuestionsRepository implements QuestionsRepository {
+  constructor(
+    private questionAttachmentsRepository: QuestionAttachmentsRepository,
+  ) {}
+
+  public items: Question[] = []
+
+  async findById(id: string) {
+    const question = this.items.find((item) => item.id.toString() === id)
+
+    if (!question) {
+      return null
+    }
+
+    return question
+  }
+
+  async delete(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+
+    this.items.splice(itemIndex, 1)
+
+    this.questionAttachmentsRepository.deletManyByQuestionId(
+      question.id.toString(),
+    )
+  }
+
+  async findBySlug(slug: string) {
+    const question = this.items.find((item) => item.slug.value === slug)
+
+    if (!question) {
+      return null
+    }
+
+    return question
+  }
+
+  async create(question: Question) {
+    this.items.push(question)
+    DomainEvents.dispatchEventsForAggregate(question.id)
+  }
+
+  async save(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+
+    this.items[itemIndex] = question
+    DomainEvents.dispatchEventsForAggregate(question.id)
+  }
+
+  async findManyRecent({ page }: PaginationParams) {
+    const questions = this.items
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20)
+
+    return questions
+  }
+}
