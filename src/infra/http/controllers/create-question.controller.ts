7bae6417a214494d9509929346061a6f6ee064fd
gbrogni <harden13brogni@gmail.com>
2024-02-21 21:11:02 -0300

    refactor: Refatora tratativa de erros nos controllers

--- src/infra/http/controllers/create-question.controller.ts
+++ src/infra/http/controllers/create-question.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { z } from 'zod'
 import { ZodValidationPipe } from '../pipes/zod-validation-pipe';
 import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard';
@@ -31,12 +31,16 @@ export class CreateQuestionController {
     const { title, content } = body
     const userId = user.sub
 
-    await this.createQuestion.execute({
+    const result = await this.createQuestion.execute({
       title,
       content,
       authorId: userId,
       attachmentIds: []
     });
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
   }
 
 }
