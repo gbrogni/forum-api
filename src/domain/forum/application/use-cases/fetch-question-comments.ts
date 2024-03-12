f68f87d4a637668435f5b985ae97f2f7c8358905
gbrogni <harden13brogni@gmail.com>
2024-03-12 20:18:25 -0300

    feat: Listando comentários com autor

--- src/domain/forum/application/use-cases/fetch-question-comments.ts
+++ src/domain/forum/application/use-cases/fetch-question-comments.ts
@@ -1,14 +1,14 @@
-import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment';
 import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository';
 import { Either, right } from '@/core/either';
 import { Injectable } from '@nestjs/common';
+import { CommentWithAuthor } from '../../enterprise/entities/value-objects/comment-with-author'
 
 interface FetchQuestionCommentsUseCaseRequest {
   questionId: string;
   page: number;
 }
 
-type FetchQuestionCommentsUseCaseResponse = Either<null, { questionComments: QuestionComment[] }>;
+type FetchQuestionCommentsUseCaseResponse = Either<null, { comments: CommentWithAuthor[] }>;
 
 @Injectable()
 export class FetchQuestionCommentsUseCase {
@@ -18,13 +18,16 @@ export class FetchQuestionCommentsUseCase {
     questionId,
     page,
   }: FetchQuestionCommentsUseCaseRequest): Promise<FetchQuestionCommentsUseCaseResponse> {
-    const questionComments =
-      await this.questionCommentsRepository.findManyByQuestionId(questionId, {
+
+    const comments = await this.questionCommentsRepository.findManyByQuestionIdWithAuthor(
+      questionId,
+      {
         page,
-      });
+      },
+    );
 
     return right({
-      questionComments,
+      comments,
     });
   }
 }
