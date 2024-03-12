e793585ee8148aded72bf3a1efbe19f210c7aae5
gbrogni <harden13brogni@gmail.com>
2024-03-12 20:41:43 -0300

    Cria value object de detalhes da pergunta

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,5 +1,6 @@
 import { PaginationParams } from '@/core/repositories/pagination-params';
 import { Question } from '@/domain/forum/enterprise/entities/question';
+import { QuestionDetails } from '../../enterprise/entities/value-objects/question-details';
 
 export abstract class QuestionsRepository {
   abstract findById(id: string): Promise<Question | null>;
@@ -8,4 +9,5 @@ export abstract class QuestionsRepository {
   abstract delete(question: Question): Promise<void>;
   abstract save(question: Question): Promise<void>;
   abstract findManyRecent(params: PaginationParams): Promise<Question[]>;
+  abstract findDetailsBySlug(slug: string): Promise<QuestionDetails | null>;
 }
